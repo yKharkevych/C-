1ed6f10bbd54032ffd278bcae7b746f4d96ae7c7
Elyzaveta Kharkevych <y.kharkevych@ukma.edu.ua>
2026-01-23 11:08:03 +0200

    new  arg

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello !");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello " + args[0] + "!");
+    }
+}
