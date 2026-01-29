40c6c53624ee61fbf0f103109f0158d01b572ea2
Elyzaveta Kharkevych <y.kharkevych@ukma.edu.ua>
2026-01-29 13:21:57 +0200

    second hw

---
+++ Budgets/Budgets/Budget.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Budgets
+{
+    class Budget
+    {
+        List<double> transactions;
+        private string name;
+
+        public Budget(string name)
+        {
+            transactions = new List<double>();
+            this.name = name;
+        }
+        public void AddTransaction(double amount)
+        {
+            transactions.Add(amount);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var lowest = 9999.99;
+            var highest = 0.0;
+            foreach (var amount in transactions)
+            {
+                result += amount;
+                lowest = Math.Min(lowest, amount);
+                highest = Math.Max(highest, amount);
+            }
+            result /= transactions.Count;
+            Console.WriteLine($"average transaction is ${result:N2}");
+            Console.WriteLine($"lowest transaction is ${lowest}");
+            Console.WriteLine($"highest transaction is ${highest}");
+        }
+    }
+}
---
+++ Budgets/Budgets/Program.cs
@@ -0,0 +1,15 @@
+﻿using Budgets;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var budget = new Budget("valet");
+
+        budget.AddTransaction(50.54);
+        budget.AddTransaction(25.5);
+        budget.AddTransaction(83.67);
+
+        budget.ShowStatistics();
+    }
+}
