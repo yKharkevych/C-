1c9dc584bcd34e0e2883d4a06e2a6731d85670f0
Elyzaveta Kharkevych <y.kharkevych@ukma.edu.ua>
2026-04-05 15:59:53 +0300

    start lab 2

---
+++ Manager/ExpenseManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Manager.ExpenseManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
